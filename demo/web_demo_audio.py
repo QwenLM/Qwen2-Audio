fdab58a839ed2d4503af3f34e3de68a89850708f
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2024-11-05 14:15:51 -0500

    Update web_demo_audio.py

--- demo/web_demo_audio.py
+++ demo/web_demo_audio.py
@@ -114,7 +114,7 @@ def predict(chatbot, task_history):
     with open('qwen2_audio_mutox_test.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
         for entry in audio_urls:
-            writer.writerow([entry["audio_url"], response])
+            writer.writerow([entry, response])
         
     task_history.append({'role': 'assistant',
                          'content': response})
